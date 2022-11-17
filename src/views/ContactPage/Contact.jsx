555a06a52a3794a635de099979739d269428ff8e
twang72 <71084257+twang72@users.noreply.github.com>
2022-11-17 12:31:36 -0800

    created the contact page with minimum information.

--- src/views/ContactPage/Contact.jsx
+++ src/views/ContactPage/Contact.jsx
@@ -1,11 +1,23 @@
 import React, { Fragment } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from "react-bootstrap/Button";
+import Card from 'react-bootstrap/Card';
+import '../../App.css';
+import ListGroup from "react-bootstrap/ListGroup";
 
-const Contact = () => (
-  <Fragment>
-    <p>
-        Contact Us!
-    </p>
-  </Fragment>
-);
+const Contact = () => {
+  return (
+    <Card style={{ width: '40rem' }} className="contact-card text-center">
+      <Card.Body>
+        <Card.Title>Contact Us</Card.Title>
+        <ListGroup horizontal className="contact-list">
+          <ListGroup.Item>Phone: +604 789 303</ListGroup.Item>
+          <ListGroup.Item>Email: example@email.com</ListGroup.Item>
+          <ListGroup.Item>Address: 410 West Georgia, Vancouver, BC</ListGroup.Item>
+        </ListGroup>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default Contact;
